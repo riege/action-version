f62ffdf7ffddb08547bfbdd3eefea0ec375533ec
Matthias Radig <radig@riege.com>
2021-05-06 12:11:15 +0200

    BUILD-52 Implement additional outputs for convenience
    - version-without-v: strips leading "v" from tags like v1.2.3
    - major, minor, patch: extracts parts of a semantic version

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const core = require('@actions/core');
-const getVersion = require('./version');
+const version = require('./version');
 
 try {
-    const version = getVersion.get(process.env.GITHUB_REF, process.env.GITHUB_RUN_NUMBER, process.env.GITHUB_SHA);
-    core.setOutput("version", version);
+    const outputs = version.get(process.env.GITHUB_REF, process.env.GITHUB_RUN_NUMBER, process.env.GITHUB_SHA);
+    core.setOutput("version", outputs.version);
+    core.setOutput("version-without-v", outputs.versionWithoutV);
+    core.setOutput("major", outputs.major);
+    core.setOutput("minor", outputs.minor);
+    core.setOutput("patch", outputs.patch);
 } catch (error) {
     core.setFailed(error.message);
 }
